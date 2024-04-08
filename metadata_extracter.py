13857b5f7a09c16f610fb06899c84335753e02c4
brayevalerien <valerien.braye@telecom-sudparis.eu>
2024-04-08 20:09:35 +0200

    wrote simple ui with place holder function

---
+++ metadata_extracter.py
@@ -0,0 +1,5 @@
+def extract_metadata(image):
+    # TODO: write the actual function instead of placeholders
+    print("extract_metadata called!")
+    print(f"Image path: {image}")
+    return "positive prompt", "negative prompt", "model name"
---
+++ webui.py
@@ -0,0 +1,19 @@
+import gradio as gr
+import metadata_extracter as emd
+
+groot = gr.Blocks(title="Prompt Retreiver")
+
+with groot:
+    with gr.Column():
+        input_image = gr.Image(type="filepath")
+        positive = gr.Text(label="Positive prompt", show_label=True, show_copy_button=True)
+        negative = gr.Text(label="Negative prompt", show_label=True, show_copy_button=True)
+        model = gr.Text(label="Model name", show_label=True, show_copy_button=True)
+        
+    input_image.change(
+        fn=emd.extract_metadata,
+        inputs=input_image, 
+        outputs=[positive, negative, model]
+    )
+        
+groot.launch(inbrowser=True, server_port=6006)
